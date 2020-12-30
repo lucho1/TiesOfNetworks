03ad6785847acc3d7abbf985300c837728bb3fb5
lucho1 <luchosuaya@hotmail.com>
2020-12-30 12:54:40 +0100

    Delivery Manager class skeleton
    Just made the skeleton for the delivery manager, with all the variables and functions, and now I just have to implement them

--- Networks - Exercises and Practices/Multiplayer Game/Engine/source/Networks/DeliveryManager.cpp
+++ Networks - Exercises and Practices/Multiplayer Game/Engine/source/Networks/DeliveryManager.cpp
@@ -1,3 +1,60 @@
 #include "Core/Core.h"
+#include "DeliveryManager.h"
 
 // TODO(you): Reliability on top of UDP lab session
+
+DeliveryMessage* DeliveryManager::WriteSequenceNumber(OutputMemoryStream& packet)
+{
+	++m_NextSeqNumOut;
+	packet << m_NextSeqNumOut;
+
+	DeliveryMessage delivery;
+	delivery.sequenceNumber = m_NextSeqNumOut;
+	delivery.dispatchTime = Time.time;
+
+	// TODO (lucho): we should register notif. callbacks into the delivery
+	// objects. Basically onSuccess/onFailure (lambdas?)
+
+	m_PendingDeliveries.push_back(delivery);
+
+	return &delivery;
+}
+
+bool DeliveryManager::ProcessSequenceNumber(const InputMemoryStream& packet)
+{
+	// TODO (lucho): Here we should check if the seq. num. arrives in order
+	// then, if it does (otherwise gets discarded), we shall add the seq. num. to a
+	// list of pending acknowledgements
+
+	// if not discarded, deserialize & process packet
+	// at the end of frame, or after interval, send a packet with all the acknowledged
+	// sequence numbers from all received packets (WritePendingSequenceNumbers)
+	return false;
+}
+
+bool DeliveryManager::HasPendingSequenceNumbers() const
+{
+	return false;
+}
+
+void DeliveryManager::WritePendingSequenceNumbers(OutputMemoryStream& packet)
+{
+}
+
+void DeliveryManager::ProcessAckdSequenceNumbers(const InputMemoryStream& packet)
+{
+	// TODO (lucho): For each seq. num., find the packet delivery info., then
+	// invoke its callbacks success/failure as needed and remove delivery
+
+	// Each frame, we check for pending deliveries for timeout (ProcessTimedOutPackets)
+}
+
+void DeliveryManager::ProcessTimedOutPackets()
+{
+	// TODO (lucho): For each delivery timed out, we shall call onFailure()
+	// and remove the delivery
+}
+
+void DeliveryManager::ClearDeliveries()
+{
+}
--- Networks - Exercises and Practices/Multiplayer Game/Engine/source/Networks/DeliveryManager.h
+++ Networks - Exercises and Practices/Multiplayer Game/Engine/source/Networks/DeliveryManager.h
@@ -2,5 +2,59 @@
 #define _DELIVERYMANAGER_H_
 
 // TODO(you): Reliability on top of UDP lab session
+class DeliveryManager;
+class DeliveryDelegate
+{
+public:
+
+	DeliveryDelegate() {}
+	~DeliveryDelegate() = default;
+
+	virtual void onDeliverySuccess(DeliveryManager* deliveryManager) = 0;
+	virtual void onDeliveryFailure(DeliveryManager* deliveryManager) = 0;
+};
+
+
+struct DeliveryMessage
+{
+	uint32 sequenceNumber = 0;
+	double dispatchTime = 0.0;
+	DeliveryDelegate* deliveryDelegate = nullptr;
+};
+
+
+class DeliveryManager
+{
+public:
+
+	DeliveryManager() {}
+	~DeliveryManager() = default;
+
+	// To write a seq. num. into a packet (senders)
+	DeliveryMessage* WriteSequenceNumber(OutputMemoryStream& packet);
+
+	// To process a seq. num. from a packet (receivers)
+	bool ProcessSequenceNumber(const InputMemoryStream& packet);
+
+	// To write acknowledged seq. nums. into a packet (receivers)
+	bool HasPendingSequenceNumbers() const;
+	void WritePendingSequenceNumbers(OutputMemoryStream& packet);
+
+	// To process acknowledged seq. nums. from a packet (senders)
+	void ProcessAckdSequenceNumbers(const InputMemoryStream& packet);
+	void ProcessTimedOutPackets();
+
+	void ClearDeliveries();
+
+private:
+
+	// Sender
+	uint32 m_NextSeqNumOut = -1;
+	std::vector<DeliveryMessage> m_PendingDeliveries;
+
+	// Receiver
+	uint32 m_NextSeqNumExpected = -1;
+	std::vector<uint32> m_PendingSequenceNums;
+};
 
 #endif //_DELIVERYMANAGER_H_
